c31cd2c0c2bb7bc8091bb849972930ee3fc717dc
Barayuda Gautama <saya.bara@gmail.com>
2018-10-25 09:41:23 +0800

    refactor to kotlin extensions

--- app/src/main/java/web/barayuda/firstkotlin/RecyclerViewAdapter.kt
+++ app/src/main/java/web/barayuda/firstkotlin/RecyclerViewAdapter.kt
@@ -5,11 +5,10 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import com.squareup.picasso.Picasso
 import kotlinx.android.extensions.LayoutContainer
 import kotlinx.android.synthetic.main.item_list.*
+import kotlinx.android.synthetic.main.item_list.view.*
 
 
 class RecyclerViewAdapter(private val context: Context, private val items: List<Item>, private
@@ -25,29 +24,16 @@ class RecyclerViewAdapter(private val context: Context, private val items: List<
 
     override fun getItemCount(): Int = items.size
 
-//    class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
-//
-//        private val name = view.findViewById<TextView>(R.id.name)
-//        private val image = view.findViewById<ImageView>(R.id.image)
-//
-//        fun bindItem(items: Item, listener: (Item) -> Unit) {
-//            name.text = items.name
-//            items.image?.let { Picasso.get().load(it).into(image) }
-//            itemView.setOnClickListener {
-//                listener(items)
-//            }
-//        }
-//    }
-
-    // cara lain menggunakan kotlin extentions, bind dengan hanya memanggil id pada XML
-
     class ViewHolder(override val containerView: View) : RecyclerView.ViewHolder(containerView),
         LayoutContainer {
-        fun bindItem(items: Item, listener: (Item) -> Unit) {
-            name.text = items.name
-            items.image?.let { Picasso.get().load(it).into(image) }
-            containerView.setOnClickListener { listener(items) }
 
+        fun bindItem(items: Item, listener: (Item) -> Unit) {
+            itemView.name.text = items.name
+            items.image?.let { Picasso.get().load(it).into(image) }
+            itemView.setOnClickListener {
+                listener(items)
+            }
         }
     }
+
 }
