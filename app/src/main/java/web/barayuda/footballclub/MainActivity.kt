3ee9c0bf3cfaa4e393b484aa1e203bdb5fee1fa1
Barayuda Gautama <saya.bara@gmail.com>
2018-10-29 16:07:07 +0800

    fix error lateinit var & remove onItemClickListener

--- app/src/main/java/web/barayuda/footballclub/MainActivity.kt
+++ app/src/main/java/web/barayuda/footballclub/MainActivity.kt
@@ -11,6 +11,7 @@ import android.widget.*
 import com.google.gson.Gson
 import org.jetbrains.anko.*
 import org.jetbrains.anko.recyclerview.v7.recyclerView
+import org.jetbrains.anko.support.v4.onRefresh
 import org.jetbrains.anko.support.v4.swipeRefreshLayout
 import web.barayuda.footballclub.Api.ApiRespository
 import web.barayuda.footballclub.Model.Team
@@ -19,44 +20,18 @@ import web.barayuda.footballclub.Util.visible
 
 class MainActivity : AppCompatActivity(), MainView {
 
-    private lateinit var listItem: RecyclerView
-    private lateinit var progressBar: ProgressBar
-    private lateinit var swipeRefresh: SwipeRefreshLayout
-    private lateinit var spinner: Spinner
-
     private var teams: MutableList<Team> = mutableListOf()
     private lateinit var presenter: MainPresenter
     private lateinit var adapter: MainAdapter
-
+    private lateinit var listTeam: RecyclerView
+    private lateinit var progressBar: ProgressBar
+    private lateinit var swipeRefresh: SwipeRefreshLayout
+    private lateinit var spinner: Spinner
     private lateinit var leagueName: String
 
     @SuppressLint("ResourceAsColor")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        adapter = MainAdapter(teams)
-        listItem.adapter = adapter
-
-        val request = ApiRespository()
-        val gson = Gson()
-        presenter = MainPresenter(this, request, gson)
-
-        val spinnerItems = resources.getStringArray(R.array.league)
-        val spinnerAdapter = ArrayAdapter(ctx, android.R.layout.simple_spinner_dropdown_item, spinnerItems)
-
-        spinner.adapter = spinnerAdapter
-
-        spinner.onItemClickListener = object : AdapterView.OnItemSelectedListener, AdapterView.OnItemClickListener {
-            override fun onItemClick(parent: AdapterView<*>?, pview1: View?, position: Int, id: Long) {}
-
-            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                leagueName = spinner.selectedItem.toString()
-                presenter.getTeamList(leagueName)
-            }
-
-            override fun onNothingSelected(parent: AdapterView<*>?) {}
-        }
 
         linearLayout {
             lparams(width = matchParent, height = wrapContent)
@@ -75,7 +50,7 @@ class MainActivity : AppCompatActivity(), MainView {
                 relativeLayout {
                     lparams(width = matchParent, height = wrapContent)
 
-                    listItem = recyclerView {
+                    listTeam = recyclerView {
                         lparams(width = matchParent, height = wrapContent)
                         layoutManager = LinearLayoutManager(ctx)
                     }
@@ -86,6 +61,30 @@ class MainActivity : AppCompatActivity(), MainView {
                         }
                 }
             }
+        }
+
+        val spinnerItems = resources.getStringArray(R.array.league)
+        val spinnerAdapter = ArrayAdapter(ctx, android.R.layout.simple_spinner_dropdown_item, spinnerItems)
+        spinner.adapter = spinnerAdapter
+
+        adapter = MainAdapter(teams)
+        listTeam.adapter = adapter
+
+        val request = ApiRespository()
+        val gson = Gson()
+        presenter = MainPresenter(this, request, gson)
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                leagueName = spinner.selectedItem.toString()
+                presenter.getTeamList(leagueName)
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+        }
+
+        swipeRefresh.onRefresh {
+            presenter.getTeamList(leagueName)
         }
     }
 
