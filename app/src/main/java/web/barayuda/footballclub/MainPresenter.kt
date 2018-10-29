a6aa087fc4d1f50d3d17f23b0aa6c1641c5bcf07
Barayuda Gautama <saya.bara@gmail.com>
2018-10-29 14:37:45 +0800

    adding business logic

---
+++ app/src/main/java/web/barayuda/footballclub/MainPresenter.kt
@@ -0,0 +1,32 @@
+package web.barayuda.footballclub
+
+import com.google.gson.Gson
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
+
+class MainPresenter(private val view: MainView,
+                    private val apiRespository: ApiRespository,
+                    private val gson: Gson) {
+
+    fun getTeamList(league: String?) {
+        view.showLoading()
+        doAsync {
+            val data = gson.fromJson(apiRespository
+                .doRequest(ApiRespository.TheSportDBApi.getTeams(league)),
+                TeamResponse::class.java
+            )
+
+            uiThread {
+                view.hideLoading()
+                view.showTeamList(data.teams)
+            }
+        }
+    }
+
+}
+
+interface MainView {
+    fun showLoading()
+    fun hideLoading()
+    fun showTeamList(data: List<Team>)
+}
